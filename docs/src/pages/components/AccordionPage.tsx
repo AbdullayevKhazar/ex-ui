8204f72462074bbd8422d67d0f0153c9d5cb25de
Abdullayev123 <xezeraa-bp216@code.edu.az>
2026-04-02 15:13:40 +0400

    Everything update

--- docs/src/pages/components/AccordionPage.tsx
+++ docs/src/pages/components/AccordionPage.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionHeader,
+} from "@khazarabdulayev/ex-ui";
 const AccordionPage = () => {
   return (
     <div className="max-w-3xl mx-auto p-6 space-y-8">
